39d6edc616fe92d9000cccf6e5400da0b4cd8404
logue <logue@hotmail.co.jp>
2018-05-22 21:54:47 +0900

    FM音源定義を追加

--- js/PSGConverter.js
+++ js/PSGConverter.js
@@ -83,21 +83,24 @@
 			name: 'Lute',
 			no: 24,
 			max: 88,
-			min: 16
+			min: 16,
+			flmml: '@3 @W32 @E1,0,24,80,16 @F1,32,72,24'
 		},
 		// 29.  Electric Guitar (muted)
 		1: {
 			name: 'Ukulele',
 			no: 28,
 			max: 88,
-			min: 16
+			min: 16,
+			fm: '@3 @W16 @E1,0,24,96,16 @F1,32,64,48'
 		},
 		// 106. Banjo
 		2: {
 			name: 'Mandorin',
 			no: 105,
 			max: 88,
-			min: 16
+			min: 16,
+			fm: '@3 @W24 @E1,0,48,36,12 @F1,32,64,64'
 		},
 		// 73.  Piccolo
 		3: {
@@ -105,20 +108,23 @@
 			no: 72,
 			max: 88,
 			min: 60,
+			fm: '@0 @E1,4,24,64,4 @F1,36,80,18 @L16,36,0,128,0,0'
 		},
 		// 78.  Shakuhachi
 		4: {
 			name: 'Roncadora',
 			no: 77,
 			max: 83,
-			min: 48
+			min: 48,
+			fm: '@3 @W56 @E1,4,36,64,6 @F1,36,72,18 @L32,36,0,128,0,0'
 		},
 		// 74.  Flute
 		5: {
 			name: 'Flute',
 			no: 73,
 			max: 83,
-			min: 48
+			min: 48,
+			fm: '@2 @E1,8,48,80,24 @F1,36,80,18 @L16,36,0,128,0,0'
 		},
 
 		// 72.  Clarinet
@@ -126,49 +132,56 @@
 			name: 'Chalumeau',
 			no: 71,
 			max: 59,
-			min: 24
+			min: 24,
+			fm: '@1 @E1,8,24,64,16 @F1,24,80,48 @L16,36,0,128,0,0'
 		},
 		// 59.  Tuba
 		18: {
 			name: 'Tuba',
 			no: 58,
 			max: 59,
-			min: 24
+			min: 24,
+			fm: '@3 @W30 @E1,4,36,70,12 @F1,36,72,48 @L16,36,0,128,0,0'
 		},
 		// 47.  Orchestral Harp
 		19: {
 			name: 'Lyre',
 			no: 46,
 			max: 88,
-			min: 16
+			min: 16,
+			fm: '@3 @W48 @E1,0,48,32,0 @F1,24,56,64'
 		},
 		// 31.  Electric Guitar (distortion)
 		20: {
 			name: 'E.Guitar',
 			no: 30,
 			max: 88,
-			min: 16
+			min: 16,
+			fm: '@3 @W6 @E1,0,200,0,15'
 		},
 		// 1.   Acoustic Grand Piano
 		21: {
 			name: 'Piano',
 			no: 0,
 			max: 88,
-			min: 16
+			min: 16,
+			fm: '@3 @W80 @E1,0,170,0,12 @F1,24,64,24 @E2,0,30,0,0'
 		},
 		// 41.  Viollin
 		22: {
 			name: 'Viollin',
 			no: 40,
 			max: 88,
-			min: 16
+			min: 16,
+			fm: '@1 @E1,8,20,110,12'
 		},
 		// 43.  Chello
 		23: {
 			name: 'Chello',
 			no: 40,
 			max: 88,
-			min: 16
+			min: 16,
+			fm: '@1 @E1,8,20,110,12'
 		},
 		// 49.  Orchestra Kit (Concert SD)
 		65: {
@@ -176,20 +189,23 @@
 			no: 48,
 			max: 38,
 			min: 38,
+			fm: '@4 @N90 @E1,0,64,0,0   @F1,64,48,12'
 		},
 		// 49.  Orchestra Kit (Gran Casa)
 		66: {
 			name: 'Bass Drum',
 			no: 48,
 			max: 35,
-			min: 35
+			min: 35,
+			fm: '@4 @N64 @E1,0,127,0,0  @F1,64,12,127'
 		},
 		// 49.  Orchestra Kit (Concert SD)
 		67: {
 			name: 'Drum',
 			no: 48,
 			max: 40,
-			min: 40
+			min: 40,
+			fm: '@4 @N90 @E1,0,64,0,0 @F1,64,48,12'
 		},
 		// 49.  Orchestra Kit (Hand Cymbal)
 		68: {
@@ -197,6 +213,7 @@
 			no: 48,
 			max: 57,
 			min: 57,
+			fm: '@4 @N0 @E1,0,50,0,0 @F1,64,127,0'
 		},
 		// 14.  Xylophone
 		77: {
@@ -204,6 +221,7 @@
 			no: 15,
 			max: 88,
 			min: 16,
+			fm: '@3 @W80 @E1,0,40,30,4 @F1,36,56,18'
 		},
 	};
 
@@ -686,6 +704,108 @@
 		var stream = ret.join("");
 
 		return isDataUri ? 'data:' + this.MIME + ';base64,' + this.base64encode(stream) : stream;
+	}
+
+	// MabiMMLをFLMMLに変換
+	PSGConverter.prototype.toFlmml = function (mmls = {}) {
+		for (var i = 0; i < mmls.length; i++) {	// iの値はパート用番号として流用
+			var p = mmls[i];
+
+			var inst = (p.inst && p.inst >= 0 && p.inst < 128) ? Math.round(p.inst) : 0;
+			var pan = (p.pan && p.pan >= 0 && p.pan < 128) ? Math.round(p.pan) : 64;
+			var track = i + 1;
+			if (!p.mml[0] && !p.mml[1] && !p.mml[2]) continue;	// MMLが入っていない場合スキップ
+			ret += '/** ' + track + ' **/' + "\n";
+			header += '$track' + track + ' = ' + this.INSTSUMENTALS[inst].flmml + ' @P' + p.pan + ' v8l4o' + default_octave + ";\n";
+
+			if (inst >= 65) {
+				// ドラムの場合、メロディ→和音１という順にMMLが再生される。和音２は無視
+				var d_mml = p.mml[0] + p.mml[1];
+				var isDram = (inst !== 77) ? true : false;
+				ret += '$track' + track + ' ' + this.flmmlTrack(d_mml, isDram) + ';' + "\n";
+			} else {
+				for (var part = 0; part < p.mml.length; part++) {
+					ret += '$track' + track + ' ' + this.flmmlTrack(p.mml[part], false) + ';' + "\n";
+				}
+			}
+		}
+		return header + ret;
+	}
+
+	PSGConverter.prototype.flmmlTrack = function (mml, isDrum) {
+		var default_octave = 5;
+		var noteRevTable = ['C', 'C+', 'D', 'D+', 'E', 'F', 'F+', 'G', 'G+', 'A', 'A+', 'B'];
+		var offset = 1; // 1オクターブ高くする。
+
+		var part = [];
+		mml_notes = mml.match(/[abcdefglnortvABCDEFGLNORTV<>][\+\#-]?[0-9]*\.?&?/g);
+		if (mml_notes == null) { return false; }
+		var cOctave = default_octave;
+		var flmml = [];
+		for (var mnid = 0; mnid < mml_notes.length; mnid++) {      // 命令ごとのループ
+			var mml_note = mml_notes[mnid];
+			// 現在のオクターブを取得
+			if (isDrum != 0) {
+				if (mml_note.match(/[oO](0|[1-9][0-9]*)/)) {
+					flmml[mnid] = '';
+				} else if (mml_note.match(/</)) {
+					flmml[mnid] = '';
+				} else if (mml_note.match(/>/)) {
+					flmml[mnid] = '';
+				} else if (mml_note.match(/[nN](0|[1-9][0-9]*)/)) {
+					flmml[mnid] = 'c';
+				} else if (mml_note.match(/([abcdefgABCDEFG])([\+\#-]?)([1-9][0-9]*|0?)(\.?)(&?)/)) {
+					flmml[mnid] = 'c' + RegExp.$3 + RegExp.$4 + RegExp.$5;
+				} else {
+					flmml[mnid] = mml_note;
+				}
+			} else {
+				if (mml_note.match(/[oO](0|[1-9][0-9]*)/)) {
+					cOctave = parseInt(RegExp.$1) + offset;
+					if (isDrum == 0) {
+						flmml[mnid] = 'o' + cOctave;
+					} else {
+						flmml[mnid] = '';
+					}
+				} else if (mml_note.match(/</)) {
+					// 不等号記号の場合、FLMMLの仕様により、記号をひっくり返す。（#OCTAVE REVERSEを使えば済むけどね）
+					cOctave = cOctave - 1;
+					if (isDrum == 0) {
+						flmml[mnid] = '>';
+					} else {
+						flmml[mnid] = '';
+					}
+				} else if (mml_note.match(/>/)) {
+					cOctave = cOctave + 1;
+					if (isDrum == 0) {
+						flmml[mnid] = '<';
+					} else {
+						flmml[mnid] = '';
+					}
+				} else if (mml_note.match(/[nN](0|[1-9][0-9]*)/)) {
+					// 音階絶対指定命令
+					var nValue = parseInt(RegExp.$1);
+					var Octave = Math.floor(nValue / 12) + offset;    // ノート値を12で割った値がオクターブ
+					var note = noteRevTable[nValue % 12];     // あまりがノート値
+					// 上で取得したオクターブが等しければオクターブ命令を省略。
+					if (cOctave == Octave) {
+						flmml[mnid] = note;
+					} else {
+						if (cOctave == Octave + 1) {
+							flmml[mnid] = ('>' + note + '<');
+						} else if (cOctave == Octave - 1) {
+							flmml[mnid] = ('<' + note + '>');
+						} else {
+							flmml[mnid] = ('o' + Octave + note + 'o' + cOctave);
+						}
+					}
+				} else {
+					// それ以外の命令は変換しない
+					flmml[mnid] = mml_note;
+				}
+			}
+		}
+		return flmml.join("");
 	}
 
 
